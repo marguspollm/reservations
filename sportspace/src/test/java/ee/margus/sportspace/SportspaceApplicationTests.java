e9817f71bee5e65a6fb314041beb1b7c0dc10ed8
Margus <98420540+marguspollm@users.noreply.github.com>
2026-03-22 19:25:01 +0200

    Add testcontiners

--- sportspace/src/test/java/ee/margus/sportspace/SportspaceApplicationTests.java
+++ sportspace/src/test/java/ee/margus/sportspace/SportspaceApplicationTests.java
@@ -2,9 +2,19 @@ package ee.margus.sportspace;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class SportspaceApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18-alpine");
+
 
 	@Test
 	void contextLoads() {
